fc2a82bb39809397ce9b21ac6c95ded524e9f0eb
Patrizio Tardiolo Bonifazi <p.tardiolobonifazi@vivasoft.it>
2022-02-04 16:16:07 +0100

    Versione 2 Managing Data

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -11,8 +13,16 @@ import { Product, products } from '../products';
 export class ProductDetailsComponent implements OnInit {
 
   product: Product | undefined;
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+    ) { }
 
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+    
   ngOnInit(): void {
     // First get the product id from the current route.
     // To access the route parameters, we use route.snapshot, 
